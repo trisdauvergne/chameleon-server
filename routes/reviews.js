e7d141a9d78eb5de89ac2661b8c3f63b3c6d0831
Josephine Hedman <josephine.hedman@gmail.com>
2021-04-02 16:24:26 +0200

    Added account page with user info and reviews components

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,7 +4,6 @@ const { getReviews,addReview, updateReviewStatus } = require('../db/repository.j
 
 router.get('/:id', async (req, res) => {
   const reviews = await getReviews(req.params.id);
-  console.log(reviews);
   res.json(reviews);
 })
 
