ea71d27b19b88212733d8f1f99a7aad8b5e1e7f4
trisdauvergne <tristan.dauvergne@gmail.com>
2021-04-03 15:53:14 +0200

    cors

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { validateUser } = require('../db/repository.js');
+require('dotenv').config();
+
+const frontend = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://trisdauvergne.github.io';
 
 router.post('/', async (req, res) => {
   const userId = await validateUser(req.body.username);
   console.log(req.body);
   if (userId) {
-    res.cookie('userId', `${userId}`, { httpOnly: false, sameSite: 'none' }).sendStatus(204);
+    res.cookie('userId', `${userId}`, { httpOnly: false, sameSite: 'none' }).setHeader('Access-Control-Allow-Origin', frontend).sendStatus(204);
   } else {
       res.sendStatus(401);
   }
